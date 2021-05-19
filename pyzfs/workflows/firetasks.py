8f781469a4b0a8a29b980f3e731b1273e70c34b2
jeng-yuantsai <tsaie79@gmail.com>
2021-05-19 11:30:17 -0400

    update

--- pyzfs/workflows/firetasks.py
+++ pyzfs/workflows/firetasks.py
@@ -17,10 +17,10 @@ class RunPyzfs(FiretaskBase):
         srun -n 2048 -c 2 python /home/tug03990/site-packages/pyzfs/examples/VASP/run.py > out (cori)
         mpiexec -n 140 python /home/tug03990/site-packages/pyzfs/examples/VASP/run.py > out (owls, efrc)
     """
-    required_params = ["zfs_cmd"]
+    required_params = ["pyzfs_cmd"]
 
     def run_task(self, fw_spec):
-        cmd = env_chk(self["zfs_cmd"], fw_spec)
+        cmd = env_chk(self["pyzfs_cmd"], fw_spec)
         logger.info("Running command: {}".format(cmd))
         return_code = subprocess.call(cmd, shell=True)
         logger.info("Command {} finished running with returncode: {}".format(cmd, return_code))

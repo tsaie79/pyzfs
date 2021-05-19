b8d40359bc0f9190ab738ddb85de54658f36f45c
jeng-yuantsai <tsaie79@gmail.com>
2021-05-19 11:23:25 -0400

    add firetask RunPyzfs

---
+++ pyzfs/workflows/fireworks.py
@@ -0,0 +1,44 @@
+from pyzfs.workflows.firetasks import RunPyzfs
+
+from fireworks import Firework
+
+from atomate.common.firetasks.glue_tasks import PassCalcLocs
+from atomate.vasp.firetasks.glue_tasks import CopyVaspOutputs
+
+class PyzfsFW(Firework):
+    def __init__(
+            self,
+            parents=None,
+            structure=None,
+            name="pyzfs",
+            prev_calc_dir=None,
+            **kwargs
+    ):
+        fw_name = "{}-{}".format(
+            structure.composition.reduced_formula if structure else "unknown", name
+        )
+
+        t = []
+
+        if prev_calc_dir:
+            t.append(
+                CopyVaspOutputs(
+                    calc_dir=prev_calc_dir,
+                    additional_files=["WAVECAR"],
+                    contcar_to_poscar=True,
+                )
+            )
+        elif parents:
+            t.append(
+                CopyVaspOutputs(
+                    calc_loc=True,
+                    additional_files=["WAVECAR"],
+                    contcar_to_poscar=True,
+                )
+            )
+        else:
+            raise ValueError("Must specify structure or previous calculation")
+
+        t.append(RunPyzfs())
+        t.append()
+    super(PyzfsFW, self).__init__(t, parents=parents, name=fw_name, **kwargs)
